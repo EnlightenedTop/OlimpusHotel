24c791a123c221b35dfede67cae2f130cc6e23c2
Popa Cristian Alexandru <81997541+EnlightenedTop@users.noreply.github.com>
2022-06-13 16:31:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,25 +3,38 @@
 ////////////DOM Selectors//////////////////////////
 /////////////////////////////////////////////////////
 
-const btn = document.querySelector(".btn-primary");
+const btnHero = document.querySelector(".btn-primary");
 const hero = document.querySelector(".hero");
+const btnFeatures = document.querySelector(".features-buttons");
+const btnAllFts = document.querySelectorAll(".btn-features");
+const txtAllBox = document.querySelectorAll(".features-modal");
 ///////////////////////////////////////////////////////
 ////////////////text dissapear and apear effect/////////
 ////////////////////////////////////////////////////////
-hero.classList.add("hidden-text");
-window.addEventListener("load", function () {
-  hero.classList.remove("hidden-text");
-});
+setTimeout(() => {
+  hero.classList.add("active");
+}, 200);
+
 ///////////////////////////////////////////
-////////////////////////////////////////////////
+/////////////////Features section modals///////////
 /////////////////////////////////////////////////
-window.setInterval(function () {
-  let randomInt = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1) + min);
-  let randomDeg = (min, (max) => Math.floor(Math.random() * (max - min)));
-  let randomColor = `rgb(${randomInt(0, 255)}, ${randomInt(
-    0,
-    255
-  )}, ${randomInt(0, 255)})`;
-  btn.style.backgroundColor = `linear-gradient(from top to left, ${randomColor} ${randomDeg}%, ${randomColor} ${randomDeg}%)`;
-}, 500);
+btnFeatures.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".btn-features");
+  txtAllBox.forEach((m) => m.classList.remove("feature-modal-active"));
+  btnAllFts.forEach((b) => b.classList.remove("btn-active"));
+  ///////////////////////////////////////////////////////////
+  clicked.classList.add("btn-active");
+  document
+    .querySelector(`.features-modal-${clicked.dataset.tab}`)
+    .classList.add("feature-modal-active");
+});
+// window.setInterval(function () {
+//   let randomInt = (min, max) =>
+//     Math.floor(Math.random() * (max - min + 1) + min);
+//   let randomDeg = (min, (max) => Math.floor(Math.random() * (max - min)));
+//   let randomColor = `rgb(${randomInt(0, 255)}, ${randomInt(
+//     0,
+//     255
+//   )}, ${randomInt(0, 255)})`;
+//   btn.style.backgroundColor = `linear-gradient(from top to left, ${randomColor} ${randomDeg}%, ${randomColor} ${randomDeg}%)`;
+// }, 500);
