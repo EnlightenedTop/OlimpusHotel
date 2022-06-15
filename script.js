72e086622e5976440d8a54c55a3ce8bda360dafa
Popa Cristian Alexandru <81997541+EnlightenedTop@users.noreply.github.com>
2022-06-15 12:32:55 +0200

    Add files via upload
    added sticky nav when intersecting with another section , worked on a bit on effects on hero section , modified html a bit , css a bit for better reading and functionality :)

--- script.js
+++ script.js
@@ -5,16 +5,23 @@
 
 const btnHero = document.querySelector(".btn-primary");
 const hero = document.querySelector(".hero");
+const heroTitle = document.querySelector(".hero-title");
+const buttons = document.querySelector(".buttons");
 const btnFeatures = document.querySelector(".features-buttons");
 const btnAllFts = document.querySelectorAll(".btn-features");
 const txtAllBox = document.querySelectorAll(".features-modal");
+const aboutSection = document.querySelector(".features-section");
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav-bar");
 ///////////////////////////////////////////////////////
 ////////////////text dissapear and apear effect/////////
 ////////////////////////////////////////////////////////
 setTimeout(() => {
-  hero.classList.add("active");
+  heroTitle.classList.add("active");
 }, 200);
-
+setTimeout(() => {
+  buttons.classList.add("buttons-active");
+}, 700);
 ///////////////////////////////////////////
 /////////////////Features section modals///////////
 /////////////////////////////////////////////////
@@ -40,3 +47,19 @@ btnFeatures.addEventListener("click", function (e) {
 //   )}, ${randomInt(0, 255)})`;
 //   btn.style.backgroundColor = `linear-gradient(from top to left, ${randomColor} ${randomDeg}%, ${randomColor} ${randomDeg}%)`;
 // }, 500);
+/////////////////////sticky navigation
+const initialCoord = aboutSection.getBoundingClientRect().top;
+window.addEventListener("scroll", function (e) {
+  if (window.scrollY > initialCoord) header.classList.add("sticky");
+  else header.classList.remove("sticky");
+});
+/////Intersection observer API
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => console.log(entry));
+};
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(aboutSection);
